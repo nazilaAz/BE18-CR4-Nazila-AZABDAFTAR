a17220a2b935b17ec2116e31402ced3ce95b094c
Nazila <azabdaftar@outlook.com>
2023-03-24 16:15:14 +0100

    read media from Database

--- readMedia.php
+++ readMedia.php
@@ -23,7 +23,7 @@ if (mysqli_num_rows($resultCD)  > 0) {
         $titleCD = $row['title'];
         $shortDescCD = $row['short_description'];
         $cardCD .= "
-<div class='card mt-3' style='width: 25rem;'>
+<div class='card mt-3'>
         <div class='row no-gutters'>
             <div class='col-sm-5'>
                  {$imgCD} 
@@ -56,7 +56,7 @@ if (mysqli_num_rows($resultDVD)  > 0) {
         $titleDVD = $row['title'];
         $shortDescDVD = $row['short_description'];
         $cardDVD .= "
-<div class='card mt-3' style='width: 25rem;'>
+<div class='card mt-3'>
         <div class='row no-gutters'>
             <div class='col-sm-5'>
                  {$imgDVD} 
@@ -89,7 +89,7 @@ if (mysqli_num_rows($resultBooks)  > 0) {
         $titleBook = $row['title'];
         $shortDescBook = $row['short_description'];
         $cardBook .= "
-<div class='card mt-3' style='width: 25rem;'>
+<div class='card mt-3'>
         <div class='row no-gutters'>
             <div class='col-sm-5'>
                  {$imgBook} 
@@ -121,7 +121,7 @@ if (mysqli_num_rows($resultBooks)  > 0) {
     <link rel="stylesheet" href="components/css/style.css">
 </head>
 
-<body>
+<body id="media">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand txtFont"><img src="https://cdn-icons-png.flaticon.com/512/9389/9389532.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> Public Library</a>
@@ -141,19 +141,84 @@ if (mysqli_num_rows($resultBooks)  > 0) {
         </div>
     </nav>
     <div class="container">
+    <a href="create.php" type="button" role="button" class="btn btn-info mt-3">
+            Add new Media to List
+        </a>
+        <div class="row row-cols-3 mt-5">
+            <!-- <a href="#books" id="submitBook"> -->
+            <div class="col">
+                <img class="img-thumbnail imgTop" src="https://img.freepik.com/free-photo/book-library-with-open-textbook_1150-5920.jpg?size=626&ext=jpg&uid=R68135291&ga=GA1.1.1556330073.1677068126&semt=sph">
+                <h5 class="text-center">Books</h5>
+            </div>
+            <!-- </a> -->
+            <!-- <a href="#cds" id="submitCd"> -->
+            <div class="col">
+                <img class="img-thumbnail imgTop" src="https://img.freepik.com/premium-photo/cds-text-wooden-blocks-financial-business-concept-brown-background_384017-5325.jpg?size=626&ext=jpg&uid=R68135291&ga=GA1.1.1556330073.1677068126&semt=sph">
+                <h5 class="text-center">CDs</h5>
+            </div>
+            <!-- </a> -->
+            <!-- <a href="#dvds" id="submitDvd"> -->
+            <div class="col">
+                <img class="img-thumbnail imgTop " src="https://img.freepik.com/free-photo/cd-with-headphones_23-2148133562.jpg?size=626&ext=jpg&uid=R68135291&ga=GA1.1.1556330073.1677068126&semt=sph">
+                <h5 class="text-center">DVDs</h5>
+            </div>
+            <!-- </a> -->
 
+        </div>
+        <div class="row row-cols-3 mt-5">
+            <div class="col" id="books">
+                <?= $cardBook ?>
+            </div>
+            <div class="col" id="Cds">
+                <?= $cardCD ?>
+            </div>
+            <div class="col" id="Dvds">
+                <?= $cardDVD ?>
+            </div>
+        </div>
 
-    
-        <h3>CDs</h3>
-        <?= $cardCD ?>
-        <hr>
-        <h3>DVDs</h3>
-        <?= $cardDVD ?>
-        <hr>
-        <h3>DVDs</h3>
-        <?= $cardBook ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <!-- <script>
+        var submitbookbtn = document.getElementById('submitBook');
+        let buttonBookClicked = false;
+        let buttonCDClicked = false;
+        let buttonDVDClicked = false;
+        submitbookbtn.addEventListener('click', function handleClick() {
+            // console.log('Submit button is clicked');
+            document.getElementById("books").style.display = "block";
+            if (buttonBookClicked) {
+                // console.log('Submit button has already clicked');
+                document.getElementById("Cds").style.display = "none";
+                document.getElementById("Dvds").style.display = "none";
+                // buttonBookClicked = true;
+            }
+            buttonBookClicked = true;
+            if(buttonCDClicked){
+                document.getElementById("books").style.display = "none";
+                document.getElementById("Dvds").style.display = "none";
+            }
+            buttonBookClicked = true;
+            if(buttonDVDClicked){
+                document.getElementById("books").style.display = "none";
+                document.getElementById("Cds").style.display = "none";
+            }
+            buttonDVDClicked = true;
+        });
+
+        // var submitCds = document.getElementById('aCds');
+        // let cdsClicked = false;
+        // submitButton.addEventListener('click', function handleClick() {
+        //     console.log('Submit button is clicked');
+        //     document.getElementById("books").style.display = "block";
+        //     if (cdsClicked) {
+        //         console.log('Submit button has already clicked');
+        //         document.getElementById("aCds").style.display = "none";
+        //         document.getElementById("aDvds").style.display = "none";
+        //     }
+        //     cdsClicked = true;
+        // });
+    </script> -->
 </body>
 
 </html>
